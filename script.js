be0445be452ee71e970f39a76817e961a450f595
Alex88cat <106343319+Alex88cat@users.noreply.github.com>
2024-05-28 18:41:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('orderForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const product = document.getElementById('product').value;
+    const quantity = document.getElementById('quantity').value;
+    const email = document.getElementById('email').value;
+
+    const subject = `Pedido de ${product}`;
+    const body = `Producto: ${product}%0ACantidad: ${quantity}%0AEmail: ${email}`;
+
+    window.location.href = `mailto:tucorreo@example.com?subject=${subject}&body=${body}`;
+});
